3bc2e9e03166ab3844fe067dd03063ae3cd0eb95
MinchinWeb <w_minchin@hotmail.com>
2024-12-21 10:11:24 -0700

    [0062] updated Gridfinity labels

--- 0062 Gridfinity Labels/tasks.py
+++ 0062 Gridfinity Labels/tasks.py
@@ -1,23 +1,40 @@
 from invoke import task
 
 LABELS = [
-    "William",
-    "{nut} M2.5",
-    "{nut} M3",
-    "{head(hex)} {bolt(4,countersunk)} M3×4",
-    "{webbolt(countersunk,hex,flip)} M3×4",
-    "{magnet} 6x2",
-    "{head(square)} {bolt(20,button)} M5×20",
+    ## Test Lables
+    # "William",
+    # "{head(square)} {bolt(50,button)}\\nM5×50",
+    # "{circle} {nut_profile} {locknut_profile}\\nTest",
+
+    ## 0062C -- Nov 24, 2024
+    # "{head(hex)} {bolt(4,countersunk)}\\nM3×4",  # DIN7991
+    # "{nut} {locknut_profile}\\nM2.5",  # DIN985, locking
+    # "{nut} {locknut_profile}\\nM3",    # DIN985, locking
+    # "{head(hex)} {bolt(10,socket)}\\nM2.5×10",
+    # "{head(hex)} {bolt(14,socket)}\\nM2.5×14",
+    # "{head(hex)} {bolt(18,socket)}\\nM2.5×18",
+    # "{circle} {box(2.0)}\\n{magnet} 6x2",
+
+    ## 0062D -- Dec 5, 2024
+    "{washer} {box(0.5)}\\nM2.5",  # M2.5x5.5x0.5
+    "{washer} {box(0.5)}\\nM3",    # M3x6x0.5
+    "{nut} {nut_profile}\\nM2.5",  # DIN934
+    "{nut} {nut_profile}\\nM3",    # DIN934
+    "{head(hex)} {bolt(6,countersunk)}\\nM3×6",  # DIN7991
+    "{head(hex)} {bolt(30,socket)}\\nM3×30",     # DIN912
+    "{head(hex)} {bolt(40,socket)}\\nM3×40",     # DIN912
+
 ]
 
 # Manual here --> https://github.com/ndevenish/gflabel/
 
 FONT = "B612"
-# BASE = "webb"
-BASE = "pred"
+BASE = "webb"
+# BASE = "pred"
 # STYLE = "embedded"
 STYLE = "embossed"
 FONT_SIZE = 6
+MARGIN = 0
 
 # OUTPUT = "labels.step"
 OUTPUT = "labels.stl"
@@ -36,7 +53,7 @@ def make_labels(ctx, vscode=False):
     all_labels = " ".join(f'"{label}"' for label in LABELS)
 
     cmd = f"gflabel --base={BASE} --font={FONT} --font-size={FONT_SIZE}"
-    cmd += f" --style={STYLE} {all_labels} -o {OUTPUT}"
+    cmd += f" --style={STYLE} --margin={MARGIN} {all_labels} -o {OUTPUT}"
 
     if vscode:
         cmd += " --vscode"
